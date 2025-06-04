7baba4a65b1bea020f3ac935ee243bdb45538d52
KaidenSmith <KdntNinja-2010@proton.me>
2025-06-04 19:32:46 +0100

    Improved landing page

--- ui/components/icon/icondefs.go
+++ ui/components/icon/icondefs.go
@@ -1,4 +1,4 @@
-// templui component icon - version: main installed by templui v0.74.1
+// templui component icon - version: v0.74.2 installed by templui v0.74.2
 package icon
 
 // This file is auto generated
