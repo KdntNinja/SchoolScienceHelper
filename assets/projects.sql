2ae9391b8f1bfabffa1a7678188d4d95de647c44
KaidenSmith <KdntNinja-2010@proton.me>
2025-06-08 14:40:24 +0100

    feat: Add unique constraint for user_id and name in projects table

--- assets/projects.sql
+++ assets/projects.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS projects (
     data JSONB NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     is_public BOOLEAN NOT NULL DEFAULT FALSE,
-    public_id TEXT UNIQUE
+    public_id TEXT UNIQUE,
+    UNIQUE(user_id, name)
 );
 -- +goose Down
 DROP TABLE IF EXISTS projects;
