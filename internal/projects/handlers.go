3e9b8d893e082714510d9e20f60e6bb4d1701c09
KaidenSmith <KdntNinja-2010@proton.me>
2025-06-15 10:25:22 +0100

    feat: Implement project and quiz management features, including creation and listing functionalities

--- internal/projects/handlers.go
+++ internal/projects/handlers.go
@@ -1,17 +1,39 @@
 package projects
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
+
+var inMemoryProjects = []Project{}
 
 // ListProjects handles GET /api/projects
 func ListProjects(w http.ResponseWriter, r *http.Request) {
-	// TODO: implement
-	w.Write([]byte("[]"))
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(inMemoryProjects)
 }
 
 // CreateProject handles POST /api/projects
 func CreateProject(w http.ResponseWriter, r *http.Request) {
-	// TODO: implement
+	var p Project
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if err := json.Unmarshal(body, &p); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p.ID = strconv.FormatInt(time.Now().UnixNano()+int64(rand.Intn(1000)), 10)
+	p.CreatedAt = time.Now().Unix()
+	p.UpdatedAt = p.CreatedAt
+	inMemoryProjects = append(inMemoryProjects, p)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(p)
 }
--- internal/quizzes/handlers.go
+++ internal/quizzes/handlers.go
@@ -1,17 +1,39 @@
 package quizzes
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
+
+var inMemoryQuizzes = []Quiz{}
 
 // ListQuizzes handles GET /api/quizzes
 func ListQuizzes(w http.ResponseWriter, r *http.Request) {
-	// TODO: implement
-	w.Write([]byte("[]"))
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(inMemoryQuizzes)
 }
 
 // CreateQuiz handles POST /api/quizzes
 func CreateQuiz(w http.ResponseWriter, r *http.Request) {
-	// TODO: implement
+	var q Quiz
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if err := json.Unmarshal(body, &q); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	q.ID = strconv.FormatInt(time.Now().UnixNano()+int64(rand.Intn(1000)), 10)
+	q.CreatedAt = time.Now().Unix()
+	q.UpdatedAt = q.CreatedAt
+	inMemoryQuizzes = append(inMemoryQuizzes, q)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(q)
 }
